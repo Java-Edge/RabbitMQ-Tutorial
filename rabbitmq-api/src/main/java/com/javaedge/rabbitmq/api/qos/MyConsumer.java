5e6121eb7f5b997b8977b59b2ef20466dd2b422c
JavaEdge <sss2855845850@gmail.com>
2025-07-05 22:47:57 +0800

    feat:新增 qos 案例

---
+++ rabbitmq-api/src/main/java/com/javaedge/rabbitmq/api/qos/MyConsumer.java
@@ -0,0 +1,33 @@
+package com.javaedge.rabbitmq.api.qos;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+
+/**
+ * 限流策略 - 自定义Consumer
+ */
+public class MyConsumer extends DefaultConsumer {
+
+    private Channel channel;
+
+    public MyConsumer(Channel channel) {
+        super(channel);
+        this.channel = channel;
+    }
+
+    @Override
+    public void handleDelivery(String consumerTag, Envelope envelope,
+                               AMQP.BasicProperties properties, byte[] body) throws IOException {
+        System.err.println("-------------Consume Message-------------");
+        System.err.println("consumerTag: " + consumerTag);
+        System.err.println("envelope: " + envelope);
+        System.err.println("properties: " + properties);
+        System.err.println("body: " + new String(body));
+        // 手工ACK，参数multiple表示不批量签收
+        channel.basicAck(envelope.getDeliveryTag(), false);
+    }
+}
